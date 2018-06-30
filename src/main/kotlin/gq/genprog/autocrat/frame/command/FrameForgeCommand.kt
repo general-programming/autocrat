f462bc16a0a3fb33a28014ad7e2c02c955b80df0
Henry <omgredstone@gmail.com>
2018-06-30 02:54:50 +0100

    Handling pipeline exceptions

--- src/main/kotlin/gq/genprog/autocrat/frame/command/FrameForgeCommand.kt
+++ src/main/kotlin/gq/genprog/autocrat/frame/command/FrameForgeCommand.kt
@@ -1,11 +1,15 @@
 package gq.genprog.autocrat.frame.command
 
+import io.github.hedgehog1029.frame.dispatcher.exception.DispatcherException
+import io.github.hedgehog1029.frame.dispatcher.exception.UsageException
 import io.github.hedgehog1029.frame.dispatcher.pipeline.IPipeline
 import io.github.hedgehog1029.frame.util.Namespace
 import net.minecraft.command.ICommand
 import net.minecraft.command.ICommandSender
 import net.minecraft.server.MinecraftServer
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.text.TextComponentString
+import net.minecraft.util.text.TextFormatting
 import java.util.*
 
 /**
@@ -43,12 +47,26 @@ class FrameForgeCommand(val pipeline: IPipeline): ICommand {
         return pipeline.aliases.toMutableList()
     }
 
+    fun ICommandSender.reply(text: String, color: TextFormatting = TextFormatting.GOLD) {
+        val component = TextComponentString(text).also {
+            it.style.color = color
+        }
+
+        this.sendMessage(component)
+    }
+
     override fun execute(server: MinecraftServer, sender: ICommandSender, args: Array<out String>) {
         val namespace = Namespace()
 
         namespace.set("server", server)
         namespace.set("sender", sender)
 
-        pipeline.call(ArrayDeque(args.asList()), namespace)
+        try {
+            pipeline.call(ArrayDeque(args.asList()), namespace)
+        } catch (ex: UsageException) {
+            sender.reply("Usage: " + this.getUsage(sender), TextFormatting.RED)
+        } catch (ex: DispatcherException) {
+            sender.reply(ex.message ?: "An unknown error occurred!", TextFormatting.RED)
+        }
     }
 }
