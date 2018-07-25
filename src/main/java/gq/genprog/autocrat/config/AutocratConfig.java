716e9713fc38f3a3840c46febad3e534ec1266ba
Henry <omgredstone@gmail.com>
2018-07-25 16:32:12 +0100

    Extra detail in comments & cleanup

--- src/main/java/gq/genprog/autocrat/config/AutocratConfig.java
+++ src/main/java/gq/genprog/autocrat/config/AutocratConfig.java
@@ -10,6 +10,8 @@ import static gq.genprog.autocrat.MetaKt.MOD_ID;
  */
 @Config(modid = MOD_ID)
 public class AutocratConfig {
+	@Config.Comment({"Controls which Autocrat modules are enabled.",
+			"Certain modules may force-disable themselves if they detect a mod with conflicting functionality."})
 	public static Modules modules = new Modules();
 	public static ClaimsModule claims = new ClaimsModule();
 	public static SleepModule sleepVote = new SleepModule();
@@ -19,19 +21,19 @@ public class AutocratConfig {
 		@Config.Comment("Enable the claims modules (factions, claims)")
 		public boolean claims = true;
 
-		@Config.Comment("Enable the fancy names module")
+		@Config.Comment("Enable the fancy names module (auto colour names, nicknames)")
 		public boolean fancyNames = true;
 
 		@Config.Comment("Enable the sleep-vote module. Disables itself if Quark or Morpheus are installed.")
 		public boolean sleepVote = true;
 
-		@Config.Comment("Enable the admin module")
+		@Config.Comment("Enable the admin module (mod-mode)")
 		public boolean admin = true;
 
 		@Config.Comment("Enables the simple home module (ender pearl at feet)")
 		public boolean simpleHome = true;
 
-		@Config.Comment("Enables the command-based homes module")
+		@Config.Comment("Enables the command-based homes module (/home, /sethome). [WIP]")
 		public boolean commandHome = false;
 	}
 
@@ -49,8 +51,4 @@ public class AutocratConfig {
 		@Config.Comment("Set the maximum size that can be restored at once via /bk restore.")
 		public int maxRestoreSize = 524288;
 	}
-
-	@Config.Comment("Value in seconds of the keep-alive timeout.")
-	@Config.RangeInt(min = 15, max = 300)
-	public static int keepAliveTimeout = 30;
 }
